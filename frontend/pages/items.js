23d781509ad7f3fed56c11575d3fd44a575d2327
yt33 <yutonglilofanda@hotmail.com>
2021-05-23 11:46:07 -0700

    associate dishes with item

---
+++ frontend/pages/items.js
@@ -0,0 +1,103 @@
+/* /frontend/pages/items.js 
+
+*/
+
+import { useQuery } from "@apollo/react-hooks";
+import { useRouter } from "next/router";
+import { gql } from "apollo-boost";
+
+import {
+	Button,
+	Card,
+	CardBody,
+	CardImg,
+	CardText,
+	CardTitle,
+	Col,
+	Row,
+} from "reactstrap";
+
+const GET_ITEM_DISHES = gql`
+	query($id: ID!) {
+		item(id: $id) {
+			id
+			name
+			dishes {
+				id
+				name
+				description
+				price
+				image {
+					url
+				}
+			}
+		}
+	}
+`;
+
+
+function Items(props) {
+	const router = useRouter();
+	const { loading, error, data } = useQuery(GET_ITEM_DISHES, {
+		variables: { id: router.query.id },
+	});
+
+	if(error) return "Error Loading Dishes";
+	if(loading) return <h1>Loading ...</h1>;
+	if(data.item) {
+		const { item } = data;
+		return (
+			<>
+				<h1>{item.name}</h1>
+				<Row>
+					{item.dishes.map((res) => (
+						<Col xs = "6" sm = "4" style = {{ padding: 0 }} key = {res.id}>
+							<Card style = {{ margin: "0 10px"}}>
+								<CardImg
+									top = {true}
+									style = {{ height: 250 }}
+									src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
+								/>
+			
+								<CardBody>	
+									<CardTitle>{res.name}</CardTitle>
+									<CardText>{res.description}</CardText>
+								</CardBody>
+				
+								<div className = "card-footer">
+									<Button outline color = "primary">
+										+ Add To Card
+									</Button>
+		
+									<style jsx>
+										{`
+											a {
+												color: white;
+											}
+											a:link {
+												text-decoration: none;
+												color: white;
+											}
+											.container-fluid {
+												margin-bottom: 30px;
+											}
+											.btn-outline-primary {
+												color: #007bff !important;
+											}
+											a.hover {
+												color: white !important;
+											}
+										`}
+									</style>
+								</div>
+							</Card>
+						</Col>
+					))}
+				</Row>
+			</>
+		);
+	}
+	return <h1>Add Dishes</h1>;
+}
+
+export default Items;
